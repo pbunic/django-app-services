abe9b1382bf5ae08baea293857bf894e77ed9fd2
pbunic <bunic8080@gmail.com>
2023-07-06 12:40:36 +0200

    Drop-down menu

---
+++ blog/static/js/nav.js
@@ -0,0 +1,8 @@
+// Drop-down menu for mobile users.
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
