3730935ac2797b121ede8f5e9cb89fbfe2e17842
pbunic <bunic8080@gmail.com>
2023-08-03 12:06:16 +0200

    Test fix

--- blog/tests.py
+++ blog/tests.py
@@ -4,12 +4,11 @@ from .models import Post
 
 class PostTestCase(TestCase):
     def setUp(self):
-        Post.objects.create(title='My Post', slug='my-post', intro='introduction', body='content')
+        Post.objects.create(title='My Post', slug='my-post', body='content')
 
     def test_post_creation(self):
         post = Post.objects.get(id=1)
         post_expected = ['My Post', 'my-post', 'introduction', 'content']
         self.assertEqual(post.title, post_expected[0])
         self.assertEqual(post.slug, post_expected[1])
-        self.assertEqual(post.intro, post_expected[2])
-        self.assertEqual(post.body, post_expected[3])
+        self.assertEqual(post.body, post_expected[2])
