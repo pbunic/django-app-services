7a321816523cfbfaf8ddcf8ea967291ceb746305
pbunic <bunic8080@gmail.com>
2023-08-05 23:48:07 +0200

    Navigation fix and pagination

--- blog/static/js/nav.js
+++ blog/static/js/nav.js
@@ -1,8 +1,10 @@
 // Drop-down menu for mobile users.
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
+  document.body.style.overflow = "hidden";
 }
 
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
+  document.body.style.overflow = "auto";
 }
--- blog/views.py
+++ blog/views.py
@@ -112,9 +112,7 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', weight='A') + \
-                SearchVector('intro', weight='B') + \
-                SearchVector('body', weight='C')
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
             search_query = SearchQuery(query)
             results = Post.published.annotate(
                 search=search_vector,
