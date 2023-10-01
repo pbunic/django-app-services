1094ee867b1284ecf7231a48a37be1b9de268d85
Predrag B <pbunic@proton.me>
2023-10-01 20:53:46 +0000

    Design changes

--- blog/static/js/grid.js
+++ blog/static/js/grid.js
@@ -1,36 +1,39 @@
 // Get the grid container element
 const gridContainer = document.getElementById("grid-container");
 
-// Calculate fixed-width
-function calculateWidth() {
+// first width and height
+function updateVars() {
+	const gridItems = gridContainer.children;
     const divWidth = gridContainer.offsetWidth;
     const calculatedWidth = (divWidth - 120) / 3
-    return calculatedWidth
+
+	// Find height of the largest
+	let maxHeight = 0;
+	for (let item of gridItems) {
+		const itemHeight = item.getBoundingClientRect().height;
+		if (itemHeight > maxHeight) {
+		    maxHeight = itemHeight;
+		}
+	}
+
+	// Set width and height of the grids
+	for (let item of gridItems) {
+		item.style.minWidth = `${calculatedWidth}px`;
+		item.style.minHeight = `${maxHeight}px`;
+	}
 }
 
-function updateRootVar() {
-    const resultVar = calculateWidth();
-    document.documentElement.style.setProperty("--pg-width", `${resultVar}px`);
+// new-width
+function updateWidth() {
+	const gridItems = gridContainer.children;
+    const divWidth = gridContainer.offsetWidth;
+    const calculatedWidth = (divWidth - 120) / 3
+
+	// Set new width
+	for (let item of gridItems) {
+		item.style.minWidth = `${calculatedWidth}px`;
+	}
 }
 
-updateRootVar();
-window.addEventListener("resize", updateRootVar);
-
-
-// Calculate largest hight and assign to containers
-// Get all grid items within the container
-const gridItems = gridContainer.children;
-
-// Find height of the largest
-let maxHeight = 0;
-for (let item of gridItems) {
-    const itemHeight = item.getBoundingClientRect().height;
-    if (itemHeight > maxHeight) {
-        maxHeight = itemHeight;
-    }
-}
-
-// Set the min-height of the grids
-for (let item of gridItems) {
-    item.style.minHeight = `${maxHeight}px`;
-}
+updateVars();
+window.addEventListener("resize", updateWidth);
