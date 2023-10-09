f8b5438340fbb91ea7dc20d2c4ed2dd03c4112ad
Predrag B <pbunic@proton.me>
2023-10-09 02:54:19 +0200

    Script fix

--- blog/static/js/blog.js
+++ blog/static/js/blog.js
@@ -1,33 +1,49 @@
-// Get the grid container element
+// Get the grid container element and items
 const gridContainer = document.getElementById("grid-container");
+const gridItems = gridContainer.children;
 
 // Width calculation function
-function setVars() {
-	const gridItems = gridContainer.children;
-    const divWidth = gridContainer.offsetWidth;
-    const calculatedWidth = (divWidth - 120) / 3
+function updateWidth() {
+    let divWidth = gridContainer.offsetWidth;
+    let calculatedWidth = (divWidth - 120) / 3
 
     // Set width depending on screen size
-    if (window.matchMedia("(min-width: 768px)").matches) {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        for (let item of gridItems) {
+            // For mobile screens
+            item.style.width = "96%";
+        }
+    } else if (window.matchMedia("(min-width: 768px)").matches) {
         for (let item of gridItems) {
             // For desktop screens
-            item.style.maxWidth = `${calculatedWidth}px`;
-        }
-    } else if (window.matchMedia("(max-width: 768px)").matches) {
-        for (let item of gridItems) {
-            // For mobile screens
-		    item.style.maxWidth = "max-content";
-        }
-    } else {
-        for (let item of gridItems) {
-            // For mobile screens
-		    item.style.maxWidth = "auto";
+            item.style.width = `${calculatedWidth}px`;
         }
     }
 }
 
-// Set initial
-setVars();
+// Height calculation function
+function updateHeight() {
+	let newHeight = 0;
+	for (let item of gridItems) {
+		let itemHeight = item.getBoundingClientRect().height;
+		if (itemHeight > newHeight) {
+		    newHeight = itemHeight;
+		}
+	}
+    for (let item of gridItems) {
+        // Set same height for every item
+	    item.style.height = `${newHeight}px`;
+	}
+}
 
-// Update dynamically
-window.addEventListener("resize", setVars);
+// Combine resizes
+function updateVars() {
+    updateWidth();
+    updateHeight();
+}
+
+// Initial sizes
+updateVars();
+
+// Update width dynamically
+window.addEventListener("resize", updateWidth);
