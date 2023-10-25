289b3dad007a173d4e09b750e142ba78184582fe
pbunic <pbunic@proton.me>
2023-10-25 02:28:19 +0200

    Entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,15 @@ echo "PostgreSQL accepts connections..."
 echo "Running migrations..."
 python manage.py migrate --no-input
 
-if ! [ -f /_ ]; then
+SU_TRUE=$(echo "import sys; from django.contrib.auth.models import User; \
+\nif User.objects.filter(is_superuser=True).exists(): \
+sys.stdout.write('true')" | python manage.py shell)
+
+if [ "$SU_TRUE" = "true" ]; then
+    echo "Superuser exists."
+else
     echo "from django.contrib.auth.models import User; User.objects.create_superuser(""'""$USER_NAME""', ""'""$USER_EMAIL""', ""'""$USER_PASSWORD""')" | python manage.py shell
-    echo "Superuser created:\n$(date)" > /_
+    echo "\nSuperuser created: $USER_NAME\n$(date)"
 fi
 
 if [ "$DJANGO_SETTINGS_MODULE" = "config.settings.production" ]; then
@@ -20,3 +26,4 @@ if [ "$DJANGO_SETTINGS_MODULE" = "config.settings.production" ]; then
 fi
 
 exec "$@"
+
